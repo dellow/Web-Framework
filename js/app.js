a4ad702a5ff9f460e03febe00da541ca134eebbe
Stewart Dellow <stewdellow@gmail.com>
2013-07-12 11:08:11 +0100

    Fixed issue with local jQuery library path

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 'use strict';
 
 requirejs.config({
-	enforceDefine: true,
     baseUrl: 'js/lib',
     paths  : {
 		app   : '../app',
 		jquery: [
-			'//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
-			'lib/jquery.1.10.2'
+			'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+			'jquery.1.10.2'
 		]
     }
 });
