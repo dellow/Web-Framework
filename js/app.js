b47f3951679e2b7268435ef3db844fa6c8d50a18
Stewart Dellow <stewdellow@gmail.com>
2013-07-12 11:02:35 +0100

    Added jQuery fallback to require

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 'use strict';
 
 requirejs.config({
+	enforceDefine: true,
     baseUrl: 'js/lib',
     paths  : {
 		app   : '../app',
-		jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min'
+		jquery: [
+			'//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+			'lib/jquery.1.10.2'
+		]
     }
 });
 
