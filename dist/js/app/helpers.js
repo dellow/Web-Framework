0e37e1a00371f407e028728fc65d6732f27a2458
Stew Dellow <stewdellow@gmail.com>
2014-07-29 17:14:53 +0100

    Grid improvements and added equal heights Helper to JS helpers

--- dist/js/app/helpers.js
+++ dist/js/app/helpers.js
@@ -9,7 +9,7 @@
  *
  */
 
- ;(function(Helpers, window, undefined){
+ ;(function(Helpers, $, window, undefined){
 	'use strict';
 
 	/**
@@ -34,7 +34,32 @@
 		return (window.innerWidth < breakpoint) ? true : false;
 	}
 
+	/**
+	 * Apply equal heights to given elements.
+	**/
+	Helpers.equal_heights = function(el, breakpoint1, breakpoint2){
+        // Check the current breakpoint.
+        if($(window).width() > breakpoint1 && $(window).width() < breakpoint2){
+            // Map all qualifying element heights to an array.
+            var heights = $('[data-eh="true"]', el).map(function(){
+                return $(this).outerHeight();
+            }).get();
+            // Get the largest value from the array.
+            var large = Math.max.apply(Math, heights);
+            // Apply the CSS height to all qualifying elements.
+            $('[data-eh="true"]', el).each(function(){
+                $(this).outerHeight(large);
+            });
+        }
+        else{
+            // Reset the height attribute to `auto` (or nothing).
+            $('[data-eh="true"]', el).each(function(){
+                $(this).css({'height': 'auto'});
+            });
+        }
+    }
+
 	// Export
 	module.exports = Helpers;
 
-}(window.Helpers = window.Helpers || {}, window));
+}(window.Helpers = window.Helpers || {}, jQuery, window));
