17465371a969696bb3d833308f81e4028a1fde8b
Stew Dellow <stewdellow@gmail.com>
2014-08-06 12:14:15 +0100

    Updated some documentation and added new meta icons

--- gulp/tasks/jshint.js
+++ gulp/tasks/jshint.js
@@ -3,9 +3,7 @@
 /* ================================================== */
 var gulp        = require('gulp'),
 	gulpif      = require('gulp-if'),
-	jshint      = require('gulp-jshint'),
-	browserSync = require('browser-sync'),
-	reload      = browserSync.reload;
+	jshint      = require('gulp-jshint')
 
 /* ================================================== */
 /* Task
@@ -14,10 +12,8 @@ gulp.task('jshint', function(){
     // Run on development only
     if(GLOBAL.is_development){
 		var ret = gulp.src(GLOBAL.dist_dir + 'js/**/*.js')
-	    .pipe(reload({stream: true, once: true}))
 	    .pipe(jshint())
-	    .pipe(jshint.reporter('jshint-stylish'))
-		.pipe(gulpif(!browserSync.active, $.jshint.reporter('fail')));
+	    .pipe(jshint.reporter('jshint-stylish'));
 
 		return ret;
 	}
