cf8a0ceb6a70799c75ce092ff9b373b6e14c8937
Stew Dellow <stewdellow@gmail.com>
2014-08-13 09:27:51 +0100

    Updated to new bundle runner. Fixed issue with app folder'

--- gulp/tasks/bundle.js
+++ gulp/tasks/bundle.js
@@ -3,7 +3,7 @@
 /* ================================================== */
 var gulp     = require('gulp'),
     sequence = require('run-sequence'),
-    gulpif   = require('gulp-if'),
+    changed  = require('gulp-changed'),
     html     = require('gulp-minify-html'),
     cache    = require('gulp-cache'),
     size     = require('gulp-size'),
@@ -13,77 +13,49 @@ var gulp     = require('gulp'),
 /* Vars
 /* ================================================== */
 var state   = (GLOBAL.args != undefined) ? GLOBAL.args : 'development',
-	release = GLOBAL.releases_dir + 'app_' + Date.now() + '_' + state + '/';
-
-var files = [
-    GLOBAL.dist_dir + 'css/main.css',
-    GLOBAL.dist_dir + 'js/build/**/*',
-    GLOBAL.dist_dir + 'js/vendor/**/*',
-    GLOBAL.dist_dir + 'images/**/*.*',
-    GLOBAL.src_dir + '*.*'
-];
-
-var html_opts = {
-    comments: true,
-    spare: true
-};
+    release = GLOBAL.releases_dir + 'app_' + Date.now() + '_' + state + '/',
+    files   = [
+        GLOBAL.dist_dir + 'css/main.css',
+        GLOBAL.dist_dir + 'js/build/**/*',
+        GLOBAL.dist_dir + 'js/vendor/**/*',
+        GLOBAL.dist_dir + 'images/**/*.*',
+        GLOBAL.src_dir + '*.*'
+    ];
 
 /* ================================================== */
 /* Task
 /* ================================================== */
-// Moves all application files to `app` directory and optimises based on
-// current state.
+// Moves all application files to `app` directory
 gulp.task('bundle', function(){
-    sequence('move-bundle', 'html-bundle', 'image-bundle');
-});
-
-// Makes a timestamped release of the `src` directory and optimises based
-// on current state.
-gulp.task('release', function(){
-    sequence('move-release', 'html-release', 'image-release');
-});
-
-
-// Bundle tasks
-gulp.task('move-bundle', function(){
     return gulp.src(files, {base: GLOBAL.src_dir})
+        .pipe(changed(GLOBAL.app_dir))
         .pipe(gulp.dest(GLOBAL.app_dir));
 });
-gulp.task('html-bundle', function(){
-    // Run on production only
-    if(GLOBAL.is_production){
-        return gulp.src([GLOBAL.app_dir + '*.html', GLOBAL.app_dir + '*.php'])
-            .pipe(html(html_opts))
-            .pipe(gulp.dest(GLOBAL.app_dir));
-    }
-});
-gulp.task('image-bundle', function(){
-    // Run on production only
-    if(GLOBAL.is_production){
-        return gulp.src(GLOBAL.app_dir + 'dist/images/**/*')
-            .pipe(cache(imagemin({
-                progressive: true,
-                interlaced: true
-            })))
-            .pipe(gulp.dest(GLOBAL.app_dir + 'dist/images'))
-            .pipe(size({title: 'images'}));
-    }
+
+// Makes a timestamped release of the `src` directory and optimises based on current state.
+gulp.task('release', function(){
+    sequence('r-move', 'r-minify', 'r-imagemin');
 });
 
-// Release tasks
-gulp.task('move-release', function(){
+// Release sub tasks
+gulp.task('r-move', function(){
     return gulp.src(files, {base: GLOBAL.src_dir})
         .pipe(gulp.dest(release));
 });
-gulp.task('html-release', function(){
+gulp.task('r-minify', function(){
     // Run on production only
     if(GLOBAL.is_production){
+        // Options
+        var opts = {
+            comments: true,
+            spare: true
+        };
         return gulp.src([GLOBAL.app_dir + '*.html', GLOBAL.app_dir + '*.php'])
-            .pipe(html(html_opts))
+            .pipe(html(opts))
             .pipe(gulp.dest(release));
     }
 });
-gulp.task('image-release', function(){
+gulp.task('r-imagemin', function(){
     // Run on production only
     if(GLOBAL.is_production){
         return gulp.src(GLOBAL.app_dir + 'dist/images/**/*')
