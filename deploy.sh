cd4477636bb2f95b14b9c87cbf86c42fdcf472b7
Stew Dellow <stewdellow@gmail.com>
2014-09-19 15:19:07 +0100

    Updates to deployment script. Now deploys based on commit range

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,58 @@
 # deploy.sh
 #
 # HOW TO USE:
-# This will take an archive of the current Git project using `git archive`.
-# It will not work if this project does not have a valid Git repo.
+# Beware about ownership. This script assumes the SSH user is also the one
+# who owns the directory you are deploying to. It is recommended you use
+# a 'deploy' user for any deployment or public facing file tasks.
 #
 # 1. Make sure you have environments setup in /deploy/environments in the form of: `production.sh` | `staging.sh` | `dev.sh`
 #
 # 2. Run
 #	bash deploy.sh <environment>
 #
-# NOTES:
-# Beware about ownership. This script assumes the SSH user is also the one
-# who owns the directory you are deploying to. By default you should use
-# a 'deploy' user on your server.
-#
 # TO DO:
 # 1. Rollback
-# 2. Shared files
 #
+
+preloader(){
+	echo -ne "$(tput setaf 3)-* (10%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 3)-*-* (20%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 3)-*-*-* (30%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 3)-*-*-*-* (40%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 3)-*-*-*-*-* (50%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 3)-*-*-*-*-*-* (60%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 3)-*-*-*-*-*-*-* (70%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 3)-*-*-*-*-*-*-*-* (80%)\r$(tput sgr0)"
+	sleep 1
+	echo -ne "$(tput setaf 2)-*-*-*-*-*-*-*-*-* (100%) Done!\r$(tput sgr0)"
+	sleep 1
+}
+
+echo -e "\n------------------------------------------------"
+# ---------------------------------------------------------------------------
+# Check this is a valid Git repo
+# ---------------------------------------------------------------------------
+if [[ ! -d ./.git ]]; then
+	echo -e "$(tput setaf 1)This is not a valid Git repository$(tput sgr0)"
+	echo -e "------------------------------------------------\n"
+	exit 1
+fi
 
 # ---------------------------------------------------------------------------
 # Vars
 # ---------------------------------------------------------------------------
 #
-set -e
 # Set the start time
 start_seconds="$(date +%s)"
+# Fresh deployment
+FRESH_DEPLOYMENT=false
 
 # ---------------------------------------------------------------------------
 # Check the environment
@@ -43,12 +70,14 @@ if (($# == 1)); then
 	if [[ -f $DEPLOY_FILE ]]; then
 		source $DEPLOY_FILE
 	else
-		echo "Could not find deploy file for $DEPLOY_ENV environment, it should be located in $DEPLOY_FILE"
+		echo -e "$(tput setaf 1)Could not find deploy file for $DEPLOY_ENV environment, it should be located in $DEPLOY_FILE.$(tput sgr0)"
+		echo -e "------------------------------------------------\n"
 		exit 1
 	fi
-	echo "Deploying $APP to $DEPLOY_ENV environment."
+	echo -e "Deploying $APP to $DEPLOY_ENV environment..."
 else
-	echo "Usage: deploy.sh "
+	echo -e "$(tput setaf 1)You have supplied to many arguments. The correct usage is: deploy.sh <environment>$(tput sgr0)"
+	echo -e "------------------------------------------------\n"
 	exit 1
 fi
 
@@ -56,17 +85,30 @@ fi
 # Vars
 # ---------------------------------------------------------------------------
 #
-CURRENT_DIR=$DEPLOY_PATH/$APP/current
+# Release name
 RELEASE_NAME=`date +"%Y-%m-%d-%H%M%S"`
-CURRENT_RELEASE=$DEPLOY_PATH/$APP/releases/$RELEASE_NAME
-REMOTE_RELEASES=$DEPLOY_PATH/$APP/releases
+# Local release location
+LOCAL_RELEASE=deploy/releases/$RELEASE_NAME.tar.tgz
+# Remote release
+REMOTE_RELEASE=$DEPLOY_PATH/release/$RELEASE_NAME.tar.tgz
+
+# ---------------------------------------------------------------------------
+# Check commit log
+# ---------------------------------------------------------------------------
+#
+if [[ ! -f ./deploy/release_commit_log ]]; then
+	echo -e "Creating releases commit log..."
+	touch ./deploy/release_commit_log
+
+	FRESH_DEPLOYMENT=true
+fi
 
 # ---------------------------------------------------------------------------
 # Check for releases directory
 # ---------------------------------------------------------------------------
 #
 if [[ ! -d ./deploy/releases ]]; then
-	echo "Creating releases directory..."
+	echo -e "Creating releases directory..."
 	mkdir -p ./deploy/releases
 fi
 
@@ -74,33 +116,62 @@ fi
 # Create a release for the Git archive
 # ---------------------------------------------------------------------------
 #
-echo "Creating release for Git and deploying tarball to server..."
-git archive --format tar --output ./deploy/releases/$RELEASE_NAME.tar.tgz $BRANCH
+echo -e "Creating release for GitHub and deploying tarball to server..."
+if [ "$FRESH_DEPLOYMENT" = true ] ; then
+	# Get latest commit from remote origin
+	COMMIT_RANGE_TO="$(git ls-remote origin | awk '/master/ {print $1}')"
+	# Get first ever commit
+	COMMIT_RANGE_FROM="$(git rev-list HEAD | tail -n 1)"
+	# Show preloader
+	#preloader
+	# Check commit range
+	if [[ ! $COMMIT_RANGE_TO = $COMMIT_RANGE_FROM ]]; then
+		# Create tarball
+		git archive --format tar --output ./deploy/releases/$RELEASE_NAME.tar.tgz $BRANCH
+	else
+		echo -e "$(tput setaf 1)Commit range is the same. Nothing to deploy.$(tput sgr0)"
+		echo -e "------------------------------------------------\n"
+		exit 1
+	fi
+else
+	# Get latest commit from remote origin
+	COMMIT_RANGE_TO="$(git ls-remote origin | awk '/master/ {print $1}')"
+	# Get latest commit from release_commit_log
+	COMMIT_RANGE_FROM="$(sed -n '1p' ./deploy/release_commit_log)"
+	# Show preloader
+	#preloader
+	# Check commit range
+	if [[ ! $COMMIT_RANGE_TO = $COMMIT_RANGE_FROM ]]; then
+		# Create tarball
+		git archive --format tar --output ./deploy/releases/$RELEASE_NAME.tar.tgz $COMMIT_RANGE_TO $(git diff --name-only $COMMIT_RANGE_FROM $COMMIT_RANGE_TO $BRANCH)
+	else
+		echo -e "$(tput setaf 1)Commit range is the same. Nothing to deploy.$(tput sgr0)"
+		echo -e "------------------------------------------------\n"
+		exit 1
+	fi
+fi
+
+# Update release_commit_log
+echo -e "$COMMIT_RANGE_TO" >> ./deploy/release_commit_log
+echo -e "$COMMIT_RANGE_FROM" >> ./deploy/release_commit_log
 
 # ---------------------------------------------------------------------------
 # Deploy to servers
 # ---------------------------------------------------------------------------
 #
 for SERVER in ${DEPLOY_SERVER[@]}; do
-	echo "Rsynching to $SERVER..."
-	rsync -v -e ssh deploy/releases/$RELEASE_NAME.tar.tgz $DEPLOY_USER@$SERVER:$DEPLOY_PATH/$APP/releases/
-	echo "Deployed tarball to: $CURRENT_RELEASE.tar.tgz"
-	ssh -t $DEPLOY_USER@$SERVER "sudo rm -rf $REMOTE_RELEASES/current &&
-		echo 'Creating new release directory...' &&
-		sudo mkdir $REMOTE_RELEASES/$RELEASE_NAME &&
-		echo 'Extracting release...' &&
-		sudo tar -xf $REMOTE_RELEASES/$RELEASE_NAME.tar.tgz --directory $REMOTE_RELEASES/$RELEASE_NAME &&
+	# Create releases directory
+	ssh -t $DEPLOY_USER@$SERVER "sudo mkdir -p $DEPLOY_PATH/releases &&
+	sudo chown -R $USER:www-data $DEPLOY_PATH/releases"
+	# Rsync release to server
+	echo -e "Rsynching to $SERVER..."
+	rsync -v -e ssh $LOCAL_RELEASE $DEPLOY_USER@$SERVER:$DEPLOY_PATH/releases
+	echo -e "Deployed tarball to: $REMOTE_RELEASE"
+	ssh -t $DEPLOY_USER@$SERVER "echo 'Extracting release...' &&
+		sudo tar -xf $REMOTE_RELEASE --directory $DEPLOY_PATH/release/ &&
 		echo 'Removing tarball...' &&
-		sudo rm $REMOTE_RELEASES/$RELEASE_NAME.tar.tgz &&
-		echo 'Creating symlink...'"
-
-	# Check the source and create the symlink
-	if ssh -t $DEPLOY_USER@$SERVER stat $CURRENT_RELEASE/releases/current \> /dev/null 2\>\&1
-        then
-            ssh -t $DEPLOY_USER@$SERVER "sudo ln -nfs $CURRENT_RELEASE/releases/current $CURRENT_DIR"
-        else
-            ssh -t $DEPLOY_USER@$SERVER "sudo ln -nfs $CURRENT_RELEASE/src $CURRENT_DIR"
-	fi
+		sudo rm -rf $REMOTE_RELEASE &&
+		sudo cp -rf $DEPLOY_PATH/release/$RELEASE_NAME $DEPLOY_PATH"
 done
 
 # ---------------------------------------------------------------------------
@@ -108,4 +179,5 @@ done
 # ---------------------------------------------------------------------------
 #
 end_seconds="$(date +%s)"
-echo "Deployment completed successfully in "$(expr $end_seconds - $start_seconds)" seconds"
+echo -e "$(tput setaf 2)Deployment completed successfully in "$(expr $end_seconds - $start_seconds)" seconds.$(tput sgr0)"
+echo -e "------------------------------------------------\n"
--- deploy/environments/master.sh
+++ deploy/environments/master.sh
@@ -1,10 +1,12 @@
 # Site name on the server
-APP=sitedomain.com
+APP=Application Name
 # Branch to deploy from
-BRANCH=master
+BRANCH=dev
 # Servers to deploy to (array)
 DEPLOY_SERVER=(127.0.0.1)
 # Webserver path
-DEPLOY_PATH=/var/www
+DEPLOY_PATH=/var/www/sitedomain.com/public_html
+# Git path to deploy from
+GIT_PATH=./src
 # SSH User
 DEPLOY_USER=deploy
