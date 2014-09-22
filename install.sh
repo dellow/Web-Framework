87df91053df4205ef3fa4f16c3e73db82f6e70d1
Stew Dellow <stewdellow@gmail.com>
2014-09-22 15:44:35 +0100

    Improved output

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@
 # clean redundant files.
 #
 
-echo "Installation in progress. Please wait..."
+echo -e "\n------------------------------------------------"
+echo -e "Installation in progress. Please wait..."
 
 # ---------------------------------------------------------------------------
 # Vars
@@ -24,7 +25,7 @@ start_seconds="$(date +%s)"
 # Remove initial `.git` directory
 # ------------------------------------------------------------------------
 #
-echo "Removing initial .git directory..."
+echo -e "Removing initial .git directory..."
 rm -rf .git/         && \
 # ------------------------------------------------------------------------
 # Remove the `.sass-cache` directory
@@ -35,13 +36,13 @@ rm -rf .sass-cache/  && \
 # Remove .gitkeep files
 # ------------------------------------------------------------------------
 #
-echo "Removing any .gitkeep files..."
+echo -e "Removing any .gitkeep files..."
 find . -name ".gitkeep" -print0 | xargs -0 rm -rf  && \
 # ------------------------------------------------------------------------
 # Initialize a new Git instance
 # ------------------------------------------------------------------------
 #
-echo "Initialize new Git instance..."
+echo -e "Initialize new Git instance..."
 git init             && \
 # ------------------------------------------------------------------------
 # Remove the clean file
@@ -49,7 +50,7 @@ git init             && \
 #
 rm install.sh        && \
 
-echo "Some questions for you:"
+echo -e "$(tput setaf 2)Some questions for you:$(tput sgr0)"
 # ------------------------------------------------------------------------
 # README
 # ------------------------------------------------------------------------
@@ -57,7 +58,7 @@ echo "Some questions for you:"
 if [[ -e ./README.md ]]; then
 	read -p "Do you need the Readme file? y/n " choice
 	if [[ $choice = "n" ]]; then
-		# Remove `README.md`
+		echo -e "$(tput setaf 3)Removing README.md...$(tput sgr0)"
 		rm ./README.md
 	fi
 	printf "\n"
@@ -69,7 +70,7 @@ fi
 if [[ -e ./src/.htaccess ]]; then
 	read -p "Do you need the htaccess file? y/n " choice
 	if [[ $choice = "n" ]]; then
-		# Remove `.htaccess`
+		echo -e "$(tput setaf 3)Removing .htaccess...$(tput sgr0)"
 		rm ./src/.htaccess
 	fi
 	printf "\n"
@@ -80,7 +81,7 @@ fi
 #
 read -p "Do you want to do an initial commit? y/n " choice
 if [[ $choice = "y" ]]; then
-	# Initial commit
+	echo -e "$(tput setaf 3)Will do initial commit after install$(tput sgr0)"
 	INITIAL_COMMIT=true
 fi
 printf "\n"
@@ -88,28 +89,28 @@ printf "\n"
 # Update / get dependencies with Bower
 # ------------------------------------------------------------------------
 #
-echo "Getting bower dependencies..."
+echo -e "Getting bower dependencies..."
 bower install                                        && \
 # ------------------------------------------------------------------------
 # Update / get dependencies with NPM
 # ------------------------------------------------------------------------
 #
-echo "Getting NPM dependencies..."
+echo -e "Getting NPM dependencies..."
 npm install                                          && \
 # ------------------------------------------------------------------------
 # Move bower dependency
 # ------------------------------------------------------------------------
 #
-echo "Moving /dist/ directory it's new home..."
+echo -e "Moving /dist/ directory to it's new home..."
 cp -r bower_components/framework-library/dist ./src  && \
 # ------------------------------------------------------------------------
 # Git commit
 # ------------------------------------------------------------------------
 #
 if [ "$INITIAL_COMMIT" = true ] ; then
-	# Add files
+	echo -e "Adding files to Git..."
 	git add --all
-	# Initial commit
+	echo -e "Committing files to Git..."
 	git commit -am "Initial commit"
 fi
 printf "\n"
@@ -117,14 +118,15 @@ printf "\n"
 # Create a dev branch
 # ------------------------------------------------------------------------
 #
+echo -e "Creating a dev branch..."
 git branch dev
-echo "Creating a dev branch"
 # ---------------------------------------------------------------------------
 # Complete
 # ---------------------------------------------------------------------------
 #
 end_seconds="$(date +%s)"
-echo "Framework installed successfully in "$(expr $end_seconds - $start_seconds)" seconds"
+echo -e "$(tput setaf 2)Framework installed successfully in "$(expr $end_seconds - $start_seconds)" seconds$(tput sgr0)"
+echo -e "------------------------------------------------\n"
 # ------------------------------------------------------------------------
 # Run a Git status
 # ------------------------------------------------------------------------
