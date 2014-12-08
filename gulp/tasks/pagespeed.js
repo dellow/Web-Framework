89c872330184850d9c755e8bc851e962cc17af00
Stew Dellow <stewdellow@gmail.com>
2014-12-08 16:08:26 +0000

    Massively improved PageSpeed

--- gulp/tasks/pagespeed.js
+++ gulp/tasks/pagespeed.js
@@ -3,7 +3,9 @@
 /* ================================================== */
 var args      = require('yargs').argv,
 	gulp      = require('gulp'),
-	pagespeed = require('psi');
+	pagespeed = require('psi'),
+	fs 		  = require('fs-extra'),
+	header    = require('gulp-header');
 
 /* ================================================== */
 /* Vars
@@ -12,9 +14,60 @@ var url  = args.url || 'http://google.com',
 	mode = args.mode || 'desktop';
 
 /* ================================================== */
+/* Functions
+/* ================================================== */
+function convert_object(obj){
+	var arr = [];
+	for (var key in obj) {
+	    if (obj.hasOwnProperty(key)) {
+	    	var k = key.replace(/([A-Z])/g, ' $1'),
+	    		k = k.replace(/^./, function(str){ return str.toUpperCase(); });
+
+	        arr.push(k + ' = ' + obj[key]);
+	    }
+	};
+
+	return arr.join(',');
+}
+
+/* ================================================== */
 /* Task
 /* ================================================== */
-gulp.task('pagespeed', pagespeed.bind(null, {
-	url: url,
-	strategy: mode
-}));
+gulp.task('pagespeed', function(){
+	pagespeed({
+		url     : url,
+		strategy: mode,
+		locale  : 'en_GB'
+	}, function(err, data){
+		// Stats template.
+		var stats = ['-----------------------------------',
+			'Date: <%= date %>',
+			'-',
+			'Total Score: <%= score %>',
+			'-',
+			'URL: <%= url %>',
+			'-',
+			'Page Title: <%= title %>',
+			'-',
+			'Page Stats:',
+			'<%= pageStats %>',
+			'-----------------------------------',
+			'',
+			''].join('\n');
+
+		// Create file
+		fs.ensureFile('./logs/pagespeed.txt', function(err){
+			// Task
+			gulp.src('./logs/pagespeed.txt')
+			.pipe(header(stats, {
+				date     : Date(),
+				pageStats: convert_object(data.pageStats).split(',').join("\r\n"),
+				score    : data.score,
+				title    : data.title,
+				url      : data.id
+			}))
+			.pipe(gulp.dest('./logs/'));
+		});
+
+	});
+});
