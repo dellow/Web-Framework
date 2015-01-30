0600216d237a01ac9dfdd6e8a7aad213e738a04d
Stew Dellow <stewdellow@gmail.com>
2015-01-30 17:28:52 +0000

    Change

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -13,8 +13,7 @@ gulp.task('watch', function(){
 	server.listen(35729, function(err){
 		var browserify = function(){gulp.start('browserify')},
 		    compass    = function(){gulp.start('compass')},
-		    imagemin   = function(){gulp.start('imagemin')},
-		    sprite     = function(){gulp.start('sprite')};
+		    imagemin   = function(){gulp.start('imagemin')};
 
 		if(err){
 			return console.log(err);
@@ -26,7 +25,5 @@ gulp.task('watch', function(){
 		watch(GLOBAL.dist_dir + 'css/scss/**/*.scss', compass);
 		// Run Imagemin on image updates
 		watch(GLOBAL.dist_dir + 'images/**/*.*', imagemin);
-		// Run Sprites on image updates
-		watch(GLOBAL.dist_dir + 'images/icons/sprite/*.png', sprite);
 	});
 });
