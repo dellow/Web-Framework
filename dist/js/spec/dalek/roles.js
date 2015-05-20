45ac7dba7110994fad60ff9a0ae477eed9e71a51
Stew Dellow <stewdellow@gmail.com>
2015-05-20 14:36:38 +0100

    Added missing tests

---
+++ dist/js/spec/dalek/roles.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * Application or Website name
+ *
+ * Copyright 2015, Author Name
+ * Some information on the license.
+ *
+ * Dalek Tests > Roles
+ *
+**/
+
+module.exports = {
+	'Header has "banner" role': function(test){
+		test.open('dist/.help/layout.html')
+			.assert.attr('.page-header', 'role', 'banner')
+			.done();
+	},
+	'Main has "main" role': function(test){
+		test.open('dist/.help/layout.html')
+			.assert.attr('.main', 'role', 'main')
+			.done();
+	},
+	'Navigation has "navigation" role': function(test){
+		test.open('dist/.help/layout.html')
+			.assert.attr('.navigation', 'role', 'navigation')
+			.done();
+	},
+	'Sidebar has "complimentary" role': function(test){
+		test.open('dist/.help/layout.html')
+			.assert.attr('.sidebar', 'role', 'complimentary')
+			.done();
+	},
+	'Footer has "contentinfo" role': function(test){
+		test.open('dist/.help/layout.html')
+			.assert.attr('.page-footer', 'role', 'contentinfo')
+			.done();
+	}
+};
