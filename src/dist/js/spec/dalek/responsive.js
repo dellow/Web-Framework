867ec873a8c9e640d8f91c6a0867087806882698
Stew Dellow <stewdellow@gmail.com>
2015-09-02 12:35:28 +0100

    Changed dalek reports directory name

--- src/dist/js/spec/dalek/responsive.js
+++ src/dist/js/spec/dalek/responsive.js
@@ -12,9 +12,9 @@
 module.exports = {
     'Test Google Responsive': function(test){
 		test.open('http://google.com')
-			.screenshot('dalek/screenshots/google-home.png')
+			.screenshot('dalek_tests/screenshots/google-home.png')
 			.resize({width: 400, height: 400})
-			.screenshot('dalek/screenshots/google-home-400x400.png')
+			.screenshot('dalek_tests/screenshots/google-home-400x400.png')
 			.done();
     }
 };
