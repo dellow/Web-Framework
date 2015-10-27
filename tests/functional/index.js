1bb85e0cd1a8cb4685968894593c60b21103c041
Stew Dellow <stewdellow@gmail.com>
2015-10-27 11:08:07 +0000

    Removed intern tests

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -5,25 +5,6 @@
  * Copyright 2015, Author Name
  * Some information on the license.
  *
- * Intern Tests > Functional > Index
+ * Tests > Functional > Index
  *
 **/
-
-define(function(require){
-	var registerSuite = require('intern!object');
-	var assert        = require('intern/chai!assert');
-
-    registerSuite({
-        name: 'index',
-        'greeting form': function(){
-            return this.remote
-                .get(require.toUrl('https://hellostew.com/'))
-                .setFindTimeout(5000)
-                .findByCssSelector('.content h1')
-                .getVisibleText()
-                .then(function(text){
-                    assert.strictEqual(text, 'Hello! I am a Web Developer working for Surefire Media in Nantwich.', 'Intro text should be set.');
-                });
-        }
-    });
-});
--- tests/unit/hello.js
+++ tests/unit/hello.js
@@ -5,24 +5,6 @@
  * Copyright 2015, Author Name
  * Some information on the license.
  *
- * Intern Tests > Unit > Hello
+ * Tests > Unit > Hello
  *
 **/
-
-define(function(require){
-	var registerSuite = require('intern!object');
-	var assert        = require('intern/chai!assert');
-	var hello         = require('tests/samples/hello');
-
-    registerSuite({
-        name: 'hello',
-
-        greet: function () {
-            assert.strictEqual(hello.greet('Murray'), 'Hello, Murray!',
-                'hello.greet should return a greeting for the person named in the first argument');
-
-            assert.strictEqual(hello.greet(), 'Hello, world!',
-                'hello.greet with no arguments should return a greeting to "world"');
-        }
-    });
-});
