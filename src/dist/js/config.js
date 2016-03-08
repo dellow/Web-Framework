92abae252561f050dcb3606b9e139a23a907f0d1
Stew Dellow <stewdellow@gmail.com>
2016-03-08 12:41:20 +0000

    Removed redundant config vars

--- src/dist/js/config.js
+++ src/dist/js/config.js
@@ -7,7 +7,5 @@
  *
 **/
 
-window.mobile_breakpoint = 768;
-window.wiselinks_enabled = true;
-window.helper_log        = (Helpers.isEmpty(window.gulp_env) || window.gulp_env == 'development') ? true : false;
-window.ga_active         = (Helpers.isEmpty(window.ga)) ? false : true;
+window.helper_log = (Helpers.isEmpty(window.gulp_env) || window.gulp_env = = 'development') ? true : false;
+window.ga_active  = (Helpers.isEmpty(window.ga)) ? false : true;
