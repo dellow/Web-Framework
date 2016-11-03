c694592cb614c41884b34e8d401752768e58d630
Stew Dellow <stewdellow@gmail.com>
2016-11-03 10:27:05 +0000

    Final master compile

---
+++ webpack.tests.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Wepback > Tests
+ *
+ * Copyright 2016, Author Name
+ * Some information on the license.
+ *
+**/
+
+var context = require.context('./src/dist/js/tests/unit/', true, /\.js$/); context.keys().forEach(context)
