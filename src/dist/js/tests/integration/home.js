95956198152d327678a23d41ff2d87a949b465d5
Stew Dellow <stewdellow@gmail.com>
2016-12-19 09:54:31 +0000

    Updates

--- src/dist/js/tests/integration/home.js
+++ src/dist/js/tests/integration/home.js
@@ -14,18 +14,6 @@ var package = require(process.cwd() + '/package.json')
 var page = '/'
 
 module.exports = {
-	before: function(browser){
-		console.log('Starting integration tests...')
-	},
-	after: function(browser){
-		console.log('Ending integration tests...')
-	},
-	beforeEach: function(browser){
-
-	},
-	afterEach: function(browser){
-
-	},
 	'Header element exists and has banner role': function(browser){
 		browser
       .url(package.config.url + page)
@@ -44,19 +32,19 @@ module.exports = {
 	},
 	'Main element exists and has main role': function(browser){
 		browser
-    .url(package.config.url + page)
-    .waitForElementVisible('body', 1000)
-      .pause(1000)
-    .assert.elementPresent('.main')
-    .verify.attributeEquals('.main', 'role', 'main')
+      .url(package.config.url + page)
+      .waitForElementVisible('body', 1000)
+        .pause(1000)
+      .assert.elementPresent('.main')
+      .verify.attributeEquals('.main', 'role', 'main')
 	},
 	'Footer element exists and has contentinfo role': function(browser){
 		browser
-    .url(package.config.url + page)
-    .waitForElementVisible('body', 1000)
-      .pause(1000)
-    .assert.elementPresent('.page-footer')
-    .verify.attributeEquals('.page-footer', 'role', 'contentinfo')
-      .end()
+      .url(package.config.url + page)
+      .waitForElementVisible('body', 1000)
+        .pause(1000)
+      .assert.elementPresent('.page-footer')
+      .verify.attributeEquals('.page-footer', 'role', 'contentinfo')
+        .end()
 	}
 }
