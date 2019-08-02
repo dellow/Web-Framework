28c28c6632faf6fc49da236e136fc420589f1dd3
Stew Dellow <stewdellow@gmail.com>
2019-08-02 17:36:43 +0100

    Fixes

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = (env, argv) => {
       rules: [
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /(node_modules)/,
           use: {
             loader: 'babel-loader',
             options: {
