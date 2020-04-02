fa424964dadc1cebd207c2381dfc2fefc62ffdf9
Stew Dellow <stewdellow@gmail.com>
2020-04-02 02:06:57 +0100

    Fixes

--- src/dist/js/common/helpers.js
+++ src/dist/js/common/helpers.js
@@ -404,6 +404,42 @@
   }
 
   /**
+   * Checks if a user is located in localStorage.
+   *
+   * @since 1.0.0
+   * @version 1.0.0
+  **/
+  Helpers.isUserLocated = function(retrieve)
+  {
+    // Check location is set in localStorage.
+    let check = (!this.isEmpty(window.localStorage['geolocation_lat']) && !this.isEmpty(window.localStorage['geolocation_lng']))
+
+    if (retrieve) {
+      return (check) ? {latitude: window.localStorage['geolocation_lat'], longitude: window.localStorage['geolocation_lng']} : {latitude: 55.3781, longitude: 3.4360}
+    }
+
+    return check
+  }
+
+  /**
+   * Determines if a string is a valid URL.
+   *
+   * @since 1.0.0
+   * @version 1.0.0
+  **/
+  Helpers.isValidURL = function(str)
+  {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i') // fragment locator
+
+    return !!pattern.test(str)
+  }
+
+  /**
    * Customised alert().
    *
    * @since 1.0.0
@@ -501,24 +537,6 @@
   }
 
   /**
-   * Checks if a user is located in localStorage.
-   *
-   * @since 1.0.0
-   * @version 1.0.0
-  **/
-  Helpers.isUserLocated = function(retrieve)
-  {
-    // Check location is set in localStorage.
-    let check = (!this.isEmpty(window.localStorage['geolocation_lat']) && !this.isEmpty(window.localStorage['geolocation_lng']))
-
-    if (retrieve) {
-      return (check) ? {latitude: window.localStorage['geolocation_lat'], longitude: window.localStorage['geolocation_lng']} : {latitude: 55.3781, longitude: 3.4360}
-    }
-
-    return check
-  }
-
-  /**
    * Calculates a percentage between two numbers.
    *
    * @since 1.0.0
@@ -529,24 +547,6 @@
     var decreaseValue = oldNumber - newNumber
 
     return (decreaseValue / oldNumber) * 100
-  }
-
-  /**
-   * Determines if a string is a valid URL.
-   *
-   * @since 1.0.0
-   * @version 1.0.0
-  **/
-  Helpers.isValidURL = function(str)
-  {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-
-    return !!pattern.test(str);
   }
 
   /**
